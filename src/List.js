c6da08f473165538ae7935f7d70c6ffcaeee6f96
charishma <charishma0609@gmail.com>
2024-05-03 12:04:44 +0530

    commit

---
+++ src/List.js
@@ -0,0 +1,13 @@
+ function List(){
+    return(
+        <div>
+            <li>
+                <ul>JAVA</ul>
+                <ul>sql</ul>
+                <ul>core JAVA</ul>
+            </li>
+
+        </div>
+    );
+}
+export default List;
---
+++ src/Question.js
@@ -0,0 +1,10 @@
+export function Question(){
+return(
+    <div>
+        <p>para</p>
+        <List />
+        <Question></Question>
+    </div>
+
+);
+}
